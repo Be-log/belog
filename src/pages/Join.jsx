21c981c3d5208709c8931ea96f36ee4d53a5d373
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-07 12:03:41 +0900

    Style: 로그인, 회원가입 공통 스타일 추출

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import Button from '../components/Button'
 import Input from '../components/Input'
-import LabelText from '../components/LabelText'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  BackgroundDiv,
+  MainSection,
+  ImageDiv,
+  PopupImage,
+  PopupText,
+  InfoDiv,
+  InfoCloseP,
+  InfoH1,
+  Form,
+  GuideTextP
+} from './ModalStyle'
 
 const Join = () => {
   const [form, setForm] = useState({
@@ -13,77 +26,76 @@ const Join = () => {
     github: '',
     description: '',
   })
+  const [guideText, setGuideText] = useState('');
 
   return (
-    <FormDiv>
-      <InfoH1>회원가입</InfoH1>
-      <form>
-        <label>
-          <Input
-            size={'small'}
-            value={form.nickname}
-            placeholder={'아이디를 입력하세요.'}
-          />
-        </label>
-        <Button color={'mint'}>중복확인</Button>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <label>
-          <Input
-            type={'password'}
-            size={'medium'}
-            value={form.password}
-            placeholder={'비밀번호를 입력하세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <label>
-          <Input
-            type={'password'}
-            size={'medium'}
-            value={form.passwordCheck}
-            placeholder={'비밀번호를 다시 입력해주세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <label>
-          <Input
-            size={'medium'}
-            value={form.email}
-            placeholder={'이메일을 입력해주세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <label>
-          <Input
-            size={'medium'}
-            value={form.github}
-            placeholder={'GitHub 아이디를 입력해주세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <label>
-          <Input
-            size={'medium'}
-            value={form.description}
-            placeholder={'간단한 블로그 소개를 입력해주세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <ButtonDiv>
-          <Button
-            type={'reset'}
-            color={'white'}
-          >
-            초기화
-          </Button>
-          <Button
-            color={'mint'}
-          >
-            회원가입
-          </Button>
-        </ButtonDiv>
-      </form>
-    </FormDiv>
+    <BackgroundDiv>
+      <MainSection>
+        <ImageDiv>
+          <PopupImage src="https://static.velog.io/static/media/undraw_joyride_hnno.fae6b95e.svg" alt="" />
+          <PopupText>환영합니다!</PopupText>
+        </ImageDiv>
+        <InfoDiv>
+          <InfoCloseP>
+            <FontAwesomeIcon icon={faXmark} />
+          </InfoCloseP>
+          <FormDiv>
+            <InfoH1>회원가입</InfoH1>
+            <Form>
+              <InputWrapper>
+                <Input
+                  size={'small'}
+                  value={form.nickname}
+                  placeholder={'아이디를 입력하세요.'}
+                />
+                <Button color={'mint'}>중복확인</Button>
+              </InputWrapper>
+              <Input
+                type={'password'}
+                size={'medium'}
+                value={form.password}
+                placeholder={'비밀번호를 입력하세요.'}
+              />
+              <Input
+                type={'password'}
+                size={'medium'}
+                value={form.passwordCheck}
+                placeholder={'비밀번호를 다시 입력해주세요.'}
+              />
+              <Input
+                size={'medium'}
+                value={form.email}
+                placeholder={'이메일을 입력해주세요.'}
+              />
+              <Input
+                size={'medium'}
+                value={form.github}
+                placeholder={'GitHub 아이디를 입력해주세요.'}
+              />
+              <Input
+                size={'medium'}
+                value={form.description}
+                placeholder={'블로그 소개를 50자 이내로 입력해주세요.'}
+              />
+            </Form>
+            <GuideTextP>{guideText}</GuideTextP>
+            <ButtonDiv>
+              <Button
+                type={'reset'}
+                color={'white'}
+              >
+                초기화
+              </Button>
+              <Button
+                color={'mint'}
+              >
+                회원가입
+              </Button>
+            </ButtonDiv>
+          </FormDiv>
+        </InfoDiv>
+      </MainSection>
+    </BackgroundDiv>
   )
 }
 
@@ -91,21 +103,19 @@ export default Join
 
 const FormDiv = styled.div`
   display: flex;
+  margin-top: 90px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `
 
-const InfoH1 = styled.p`
-  color: white;
-  margin-top: 0;
-  margin-bottom: 20px;
-  font-size: 30px;
-  font-weight: bold;
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `
 
 const ButtonDiv = styled.div`
-  margin-top: 25px;
+  margin-top: 20px;
   display: flex;
   justify-content: center;
   gap: 10px;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,51 +2,72 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import Button from '../components/Button'
 import Input from '../components/Input'
-import LabelText from '../components/LabelText'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  BackgroundDiv,
+  MainSection,
+  ImageDiv,
+  PopupImage,
+  PopupText,
+  InfoDiv,
+  InfoCloseP,
+  InfoH1,
+  Form,
+  GuideTextP
+} from './ModalStyle'
 
 const Login = () => {
   const [form, setForm] = useState({
     nickname: '',
     password: '',
   })
+  const [guideText, setGuideText] = useState('');
 
   return (
-    <FormDiv>
-      <InfoH3>로그인</InfoH3>
-      <form>
-        <label>
-          <Input
-            size={'medium'}
-            value={form.nickname}
-            placeholder={'아이디를 입력하세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-        <label>
-          <Input
-            type={'password'}
-            size={'medium'}
-            value={form.password}
-            placeholder={'비밀번호를 입력하세요.'}
-          />
-        </label>
-        <LabelText>안내 문자열 내려주기</LabelText>
-
-        <ButtonDiv>
-          <Button
-            type={'reset'}
-            color={'white'}
-          >
-            회원가입
-          </Button>
-          <Button
-            color={'mint'}
-          >
-            로그인
-          </Button>
-        </ButtonDiv>
-      </form>
-    </FormDiv>
+    <BackgroundDiv>
+      <MainSection>
+        <ImageDiv>
+          <PopupImage src="https://static.velog.io/static/media/undraw_joyride_hnno.fae6b95e.svg" alt="" />
+          <PopupText>환영합니다!</PopupText>
+        </ImageDiv>
+        <InfoDiv>
+          <InfoCloseP>
+            <FontAwesomeIcon icon={faXmark} />
+          </InfoCloseP>
+          <FormDiv>
+            <InfoH1>로그인</InfoH1>
+            <Form>
+              <Input
+                size={'medium'}
+                value={form.nickname}
+                placeholder={'아이디를 입력하세요.'}
+              />
+              <Input
+                type={'password'}
+                size={'medium'}
+                value={form.password}
+                placeholder={'비밀번호를 입력하세요.'}
+              />
+            </Form>
+            <GuideTextP>{guideText}</GuideTextP>
+            <ButtonDiv>
+              <Button
+                type={'reset'}
+                color={'white'}
+              >
+                회원가입
+              </Button>
+              <Button
+                color={'mint'}
+              >
+                로그인
+              </Button>
+            </ButtonDiv>
+          </FormDiv>
+        </InfoDiv>
+      </MainSection>
+    </BackgroundDiv>
   )
 }
 
@@ -54,17 +75,10 @@ export default Login
 
 const FormDiv = styled.div`
   display: flex;
-  margin-top: 100px;
+  margin-top: 160px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
-
-const InfoH3 = styled.h3`
-  color: white;
-  font-size: 30px;
-  font-weight: bold;
-  margin-bottom: 20px;
 `
 
 const ButtonDiv = styled.div`
