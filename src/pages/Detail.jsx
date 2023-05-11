ba935663293d86aaff80420012e946182c71b0f6
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-11 09:06:27 +0900

    Feat: 게시글 상세, 댓글 기능 추가 중

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,32 +1,45 @@
 import { faCircleLeft, faCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from 'axios'
+import React, { useRef, useState } from 'react'
+import { Cookies } from 'react-cookie'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
-import { useMutation } from 'react-query'
 import { useLocation } from 'react-router-dom'
-import { Cookies } from 'react-cookie'
-import React, { useState } from 'react'
-import { addReplys } from '../api/posts'
-import axios from 'axios'
 import styled from 'styled-components'
+import { addReplys, editReplys } from '../api/posts'
 import Button from '../components/Button'
 import Image from '../components/Image'
 import Navbar from './Navbar'
 
 const Detail = () => {
   const [replyComment, setReplyComment] = useState('')
+  const [editReplyComment, setEditReplyComment] = useState(false)
+  const [getCommentId, setGetCommentId] = useState(0)
   const location = useLocation()
   const navigate = useNavigate()
+  const replyInputRef = useRef(null)
+  const queryClient = useQueryClient()
   const getLoginInfo = useSelector((state) => state.loginUser)
   const cookies = new Cookies()
 
   const { postComment, ...post } = location.state.post.post
-  console.log(postComment)
+  const currentPostId = location.pathname.slice(8)
 
+  // * 해당 게시글의 댓글 리스트 비동기 조회
+  const { data } = useQuery('getCommentList', async () => {
+    const replyList = await axios.get(`http://3.34.52.229/api/posts/${currentPostId}/comments`)
+    return replyList.data
+  })
+
+  // * 게시글 삭제 click (테스트 필요)
   const deletePage = async () => {
     if (window.confirm('게시글을 삭제하시겠습니까?')) {
-      await axios.patch(`http://3.34.52.229/api/posts/${postId}`)
-
+      await axios.patch(`http://3.34.52.229/api/posts/${currentPostId}`)
+      .then(() => {
+        queryClient.invalidateQueries('getCommentList')
+      })
     }
   }
 
@@ -38,7 +51,8 @@ const Detail = () => {
   // * 댓글 등록 useMutation
   const addReplyMutation = useMutation(addReplys, {
     onSuccess: (response) => {
-      console.log('성공뮤테이션~', response)
+      setReplyComment('')
+      queryClient.invalidateQueries('getCommentList')
     }
   })
 
@@ -50,7 +64,7 @@ const Detail = () => {
       return
     } else {
       const replyInfo = {
-        postId: location.pathname.slice(8),
+        postId: currentPostId,
         comment: replyComment,
         accesstoken: cookies.get('accesstoken'),
         refreshtoken: cookies.get('refreshtoken'),
@@ -59,6 +73,63 @@ const Detail = () => {
     }
   }
 
+  // * 댓글 수정 글자 click
+  const setEditmodeHandler = (commentId, comment) => {
+    setEditReplyComment(true)
+    setGetCommentId(commentId)
+    setReplyComment(comment)
+    replyInputRef.current.focus()
+  }
+
+  // * 댓글 수정 useMutation
+  const editReplyMutation = useMutation(editReplys, {
+    onSuccess: (response) => {
+      setEditReplyComment(false)
+      setReplyComment('')
+      queryClient.invalidateQueries('getCommentList')
+    }
+  })
+
+  // * 댓글 수정 등록 버튼 click
+  const editReplySubmitHandler = (e) => {
+    e.preventDefault()
+    setEditReplyComment(true)
+    if (!replyComment) {
+      alert('댓글을 입력해주세요.')
+      return
+    } else {
+      const editReplyInfo = {
+        postId: currentPostId,
+        commentId: getCommentId,
+        comment: replyComment,
+        accesstoken: cookies.get('accesstoken'),
+        refreshtoken: cookies.get('refreshtoken'),
+      }
+      editReplyMutation.mutate(editReplyInfo)
+    }
+  }
+  
+  // * 댓글 삭제 click
+  const deleteReplyHandler = async (commentId) => {
+    let deleteChk = window.confirm(`댓글을 삭제하시겠습니까?`);
+    if (!!deleteChk) {
+      await axios.delete(`http://3.34.52.229/api/posts/${currentPostId}/comments/${commentId}`, {
+        headers: {
+          accesstoken: cookies.get('accesstoken'),
+          refreshtoken: cookies.get('refreshtoken'),
+        }
+      })
+      .then(response => {
+        alert(`댓글이 삭제되었습니다.`)
+        queryClient.invalidateQueries('getCommentList')
+      })
+      .catch(error => {
+        console.error('axios deleteReply Error', error);
+      })
+    }
+  }
+
+  // * 이전 글 / 다음 글 이동 click
   const movementClickHandler = async (movementPostId) => {
     if (!movementPostId) {
       alert('작성된 글이 없습니다.')
@@ -72,6 +143,7 @@ const Detail = () => {
       })
     }
   }
+
   return (
     <>
       <Navbar />
@@ -159,68 +231,84 @@ const Detail = () => {
           {/* 댓글 */}
           <div>
             {/* 댓글 작성 */}
-            <ReplyTotalP>{postComment.length}개의 댓글</ReplyTotalP>
+            {/* <ReplyTotalP>{data.comments.length}개의 댓글</ReplyTotalP> */}
             <form>
               <ReplyTextarea
                 name="comment"
                 value={replyComment}
                 onChange={replyTxtAreaChange}
+                ref={replyInputRef}
                 placeholder="댓글을 작성하세요"
                 cols="115"
                 rows="5"
               />
               <FormButtonDiv>
-                <Button
-                  color={'mint'}
-                  onClick={addReplyClickHandelr}
-                >
-                  댓글 작성
-                </Button>
+                {
+                  !editReplyComment &&
+                  <Button
+                    color={'mint'}
+                    onClick={addReplyClickHandelr}
+                  >
+                    댓글 작성
+                  </Button>
+                }
+                {
+                  !!editReplyComment &&
+                  <Button
+                    color={'mint'}
+                    onClick={editReplySubmitHandler}
+                  >
+                    댓글 수정
+                  </Button>
+                }
               </FormButtonDiv>
             </form>
             {/* 댓글 리스트 보기 */}
             {
-              postComment.map((reply) => {
-                return (
-                  <ReplyList key={reply.commentId}>
-                    <div className="Reply">
-                      <ReplyInfo>
-                        <Image
-                          src={`${process.env.PUBLIC_URL}/images/default_profile.png`}
-                          width={'65'}
-                          height={'65'}
-                        />
-                        <ReplyWriterInfo>
-                          <div>
-                            <ReplyNameP>{reply.nickname}</ReplyNameP>
-                            <ReplyDescP>
-                            {
-                              `${reply.createdAt.slice(0, 4)}년
+              data && data.comments &&
+              data.comments.map((reply) => (
+                <ReplyList key={reply.commentId}>
+                  <div className="Reply">
+                    <ReplyInfo>
+                      <Image
+                        src={`${process.env.PUBLIC_URL}/images/default_profile.png`}
+                        width={"65"}
+                        height={"65"}
+                      />
+                      <ReplyWriterInfo>
+                        <div>
+                          <ReplyNameP>{reply.nickname}</ReplyNameP>
+                          <ReplyDescP>
+                            {`${reply.createdAt.slice(0, 4)}년
                               ${reply.createdAt.slice(5, 7)}월
                               ${reply.createdAt.slice(8, 10)}일
-                              `
-                            }
-                            </ReplyDescP>
-                          </div>
-                          <EditReplyDiv>
-                          {
-                            getLoginInfo.nickname === reply.nickname &&
+                              `}
+                          </ReplyDescP>
+                        </div>
+                        <EditReplyDiv>
+                          {getLoginInfo.nickname === reply.nickname && (
                             <>
-                              <EditReplySpan>수정</EditReplySpan>
-                              <EditReplySpan>삭제</EditReplySpan>
+                              <EditReplySpan
+                                onClick={() => setEditmodeHandler(reply.commentId, reply.comment)}
+                              >
+                                수정
+                              </EditReplySpan>
+                              <EditReplySpan
+                                onClick={() => deleteReplyHandler(reply.commentId)}
+                              >
+                                삭제
+                              </EditReplySpan>
                             </>
-                          }
-                          </EditReplyDiv>
-                        </ReplyWriterInfo>
-                      </ReplyInfo>
-                      <ReplyContent>
-                        <p>{reply.comment}</p>
-                      </ReplyContent>
-                    </div>
-                  </ReplyList>
-                )
-              })
-            }
+                          )}
+                        </EditReplyDiv>
+                      </ReplyWriterInfo>
+                    </ReplyInfo>
+                    <ReplyContent>
+                      <p>{reply.comment}</p>
+                    </ReplyContent>
+                  </div>
+                </ReplyList>
+              ))}
           </div>
         </MainContentDiv>
       </WrapMain>
@@ -377,11 +465,6 @@ const AnotherPost = styled.span`
   color: lightgrey;
 `
 
-const otherPostNone = styled.span`
-  font-size: 13px;
-  color: #797979;
-`
-
 const AnotherPostTitle = styled.span`
   font-size: 16px;
 `
@@ -397,7 +480,7 @@ const ReplyList = styled.div`
 `
 
 const ReplyTotalP = styled.p`
-  margin-left: 3px;
+  margin: 0px 0 50px 0px;
   font-size: 20px;
   font-weight: bold;
 `
@@ -408,7 +491,7 @@ const FormButtonDiv = styled.div`
 `
 
 const ReplyTextarea = styled.textarea`
-  margin: 20px 0;
+  margin-bottom: 10px;
   padding: 30px;
   resize: none;
   background-color: #1E1E1E;
