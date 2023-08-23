b51ebea6f0cd603c2d7c24b24640fb120b355abe
hansololiviakim <dev.hansolkim@gmail.com>
2023-08-24 03:10:59 +0900

    Refactor: API 분리

--- src/api/users.js
+++ src/api/users.js
@@ -1,43 +1,41 @@
-import axios from 'axios';
+import api from './interceptor';
 
 // * 회원가입 ID 중복체크
-const idDoubleChk = async (newNickname) => {
-  try {
-    const response = await axios.get(`http://13.125.98.73:3000/api/auth/checkNickname`, newNickname);
-    return response.data;
-  } catch (error) {
-    console.error('axios idDoubleChk Error', error);
-    throw error;
-  }
+export const idDoubleChk = async (newNickname) => {
+  return api
+    .post(`/api/auth/checkNickname`, newNickname)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 
 // * 회원가입
-const addUsers = async (newUser) => {
-  try {
-    const response = await axios.post(`http://13.125.98.73:3000/api/auth/signup`, newUser);
-    return response.data;
-  } catch (error) {
-    console.error('axios addUsers Error', error);
-    throw error;
-  }
+export const addUsers = (newUser) => {
+  return api
+    .post(`/api/auth/signup`, newUser)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 
 // * 로그인
-const loginUsers = async (loginUser) => {
+export const loginUsers = async (loginUser) => {
   try {
     let setResponse = {
       getAccesstoken: '',
       getRefreshtoken: '',
     };
 
-    await axios
-      .post(`http://13.125.98.73:3000/api/auth/login`, loginUser)
+    api
+      .post(`/api/auth/login`, loginUser)
       .then((response) => {
         setResponse.getAccesstoken = response.data.accesstoken;
         setResponse.getRefreshtoken = response.data.refreshtoken;
       })
       .catch((error) => {
-        console.error(error.response);
+        throw error;
       });
 
     if (setResponse.getAccesstoken) {
@@ -47,8 +45,9 @@ const loginUsers = async (loginUser) => {
           refreshtoken: `Bearer ${setResponse.getRefreshtoken}`,
         },
       };
-      await axios
-        .get(`http://13.125.98.73:3000/api/auth/profile`, loginToken)
+
+      api
+        .get(`/api/auth/profile`, loginToken)
         .then((response) => {
           response.data.userInfo = {
             ...response.data.userInfo,
@@ -57,18 +56,16 @@ const loginUsers = async (loginUser) => {
           setResponse = { ...setResponse, ...response.data };
         })
         .catch((error) => {
-          console.error(error.response);
+          throw error;
         });
     } else if (setResponse.getAccesstoken === '' || setResponse.getAccesstoken === null) {
       alert('로그인 중 오류가 발생했습니다.');
-      return null; // 로그인 중 오류가 발생한 경우 null을 반환합니다.
+      return null;
     }
 
     return setResponse;
   } catch (error) {
     console.error('axios loginUsers Error', error);
-    throw error; // 예외를 다시 던져서 처리할 수 있도록 합니다.
+    throw error;
   }
 };
-
-export { idDoubleChk, addUsers, loginUsers };
