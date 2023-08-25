117322a17080424bf00a0fc9edb6fa56e714d9ef
hansololiviakim <dev.hansolkim@gmail.com>
2023-08-26 00:20:03 +0900

    [delete] delete all files before refactoring

--- src/components/EditerView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Viewer } from '@toast-ui/react-editor';
-
-function EditerView({ markdown }) {
-  // console.log('mark : ', typeof (markdown))
-  return <Viewer theme="dark" initialValue={markdown} />;
-}
-
-export default EditerView;
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Button from './Button';
-import Image from './Image';
-import Navbar from './Navbar';
-
-export { Button, Image, Navbar };
