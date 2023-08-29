bcf63afa3d3713713b0c73166c252f27ce7070c5
hansololiviakim <dev.hansolkim@gmail.com>
2023-08-30 02:55:55 +0900

    [feat] abstraction of http request method function

--- client/src/api/interceptor.ts
+++ client/src/api/interceptor.ts
@@ -1,13 +1,14 @@
-import axios, { AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios'
+import { paramsType, urlType } from '../interfaces/apiTypes'
 
-// * create instance
+// * [axios instance] create instance
 export const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 })
 
-// * request interceptor
+// * [axios instance] request interceptor
 api.interceptors.request.use((config: AxiosRequestConfig): InternalAxiosRequestConfig => {
   const token = 'token' || 'getSomething'
 
@@ -19,3 +20,47 @@ api.interceptors.request.use((config: AxiosRequestConfig): InternalAxiosRequestC
     },
   } as InternalAxiosRequestConfig
 })
+
+// * [HTTP Req Method] get
+export const getRequest = async ({ url, params = {} }: paramsType) => {
+  try {
+    const response = await api.get(url, params)
+    return response.data
+  } catch (error) {
+    console.log('error occurred', error)
+    throw error
+  }
+}
+
+// * [HTTP Req Method] post
+export const postRequest = async ({ url, params }: paramsType) => {
+  try {
+    const response = await api.post(url, params)
+    return response.data
+  } catch (error) {
+    console.log('error occurred', error)
+    throw error
+  }
+}
+
+// * [HTTP Req Method] put
+export const putRequest = async ({ url, params }: paramsType) => {
+  try {
+    const response = await api.put(url, params)
+    return response.data
+  } catch (error) {
+    console.log('error occurred', error)
+    throw error
+  }
+}
+
+// * [HTTP Req Method] delete
+export const deleteRequest = async ({ url }: urlType) => {
+  try {
+    const response = await api.delete(url)
+    return response.data
+  } catch (error) {
+    console.log('error occurred', error)
+    throw error
+  }
+}
